651bef280883ffd7053c6f7e6ae6decf4516c5ef
Megan Jones <38929577+meginks@users.noreply.github.com>
2019-04-24 12:27:43 -0500

    tried to solve delete timer problem. found front end solution that will work better

--- configuration/deleteVoidNotes.js
+++ configuration/deleteVoidNotes.js
@@ -1,11 +1,23 @@
 const db = require('../database/dbConfig.js');
+const cron = require('node-cron');
 const moment = require('moment');
 
 function deleteVoidNotes(note) { 
     const { id } = note.id;
     if (/*day on timestamp*/ - note.days_to_destruct === 0) {
    db('notes').where({id}).del()    
-   } else end();
+   } else end(); 
 } 
+
+
+const task = cron.schedule('* * 12 * * *', () => {
+console.log('running every 12 hours'); 
+
+
+
+})
+
+
+
 
 module.exports = { deleteVoidNotes };
