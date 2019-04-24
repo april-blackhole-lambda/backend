04cc64609f38b63f3c80d676d18ea5cea7c0fac4
Megan Jones <38929577+meginks@users.noreply.github.com>
2019-04-24 10:08:26 -0500

    added moment

--- configuration/deleteVoidNotes.js
+++ configuration/deleteVoidNotes.js
@@ -1,8 +1,9 @@
 const db = require('../database/dbConfig.js');
+const moment = require('moment');
 
 function deleteVoidNotes(note) { 
     const { id } = note.id;
-    if (/* day on the timestamp */ - note.days_to_destruct === 0) {
+    if (/*day on timestamp*/ - note.days_to_destruct === 0) {
    db('notes').where({id}).del()    
    } else end();
 } 
